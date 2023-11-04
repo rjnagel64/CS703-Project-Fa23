777c2a6c5a3fc89aad71d347c79d7632c86e4bb0
Robert J Nagel <rjnagel64@gmail.com>
2023-11-03 23:32:08 -0500

    VM: bytecode for local vars

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,18 @@ use syntax::{Expr, BinOp, Stmt, Block};
 
 #[derive(Debug, Clone, Copy)]
 enum Insn {
+    Halt,
     Literal(i64),
     Add,
     Mul,
     Print,
+    Enter(usize), // create a stack frame with n locals
+    Exit, // pop that stack frame
+    GetLocal(usize), // retrieve local variable from current frame
+    SetLocal(usize), // update local variable in current frame
 }
 
 fn compile_exp(e: &Expr, code: &mut Vec<Insn>) {
-    // write output bytes to `code`.
     match e {
         Expr::Var(x) => todo!("compile var exp"),
         Expr::Num(i) => code.push(Insn::Literal(*i)),
@@ -39,8 +43,14 @@ fn compile_block(b: &Block, code: &mut Vec<Insn>) {
 
 fn compile_stmt(s: &Stmt, code: &mut Vec<Insn>) {
     match s {
-        // How to store local variables at runtime?
-        // With a stack of "Frame", where each Frame has a vector of slots for locals?
+        // TODO: Figure out how to assign slots in the local frame.
+        // I think I will need to thread through some extra state, that maps variable names to
+        // stack slots (This is also necessary to compile variable expressions)
+        // Generating a new slot for every assignment is technically *correct*... but certainly not
+        // optimally efficient. Trying to minimize the number of slots used basically boils down to
+        // register allocation, but without needing to worry about register spills.
+        // (minimum number of local slots required == max number of variables simulatenously live?
+        // Sounds reasonable. Compute liveness with reaching definition dataflow? Maybe.)
         Stmt::Assign(x, e) => todo!("compile assign stmt"),
         Stmt::Print(e) => {
             compile_exp(e, code);
@@ -58,35 +68,62 @@ struct VM {
     stack: Vec<i64>,
     pc: usize,
     code: Vec<Insn>,
+    locals: Vec<i64>,
 }
 
 impl VM {
     pub fn new(code: Vec<Insn>) -> Self {
-        VM { stack: Vec::new(), pc: 0, code: code }
+        VM { stack: Vec::new(), pc: 0, code: code, locals: Vec::new() }
     }
 
     fn step(&mut self) -> Option<usize> {
         let insn = self.code[self.pc];
         match insn {
-            Insn::Literal(i) => { self.stack.push(i); Some(self.pc + 1) },
+            Insn::Halt => return None,
+            Insn::Literal(i) => {
+                self.stack.push(i);
+            },
             Insn::Add => {
                 let y = self.stack.pop().unwrap();
                 let x = self.stack.pop().unwrap();
                 self.stack.push(x + y);
-                Some(self.pc + 1)
             },
             Insn::Mul => {
                 let y = self.stack.pop().unwrap();
                 let x = self.stack.pop().unwrap();
                 self.stack.push(x * y);
-                Some(self.pc + 1)
             },
             Insn::Print => {
                 let x = self.stack.pop().unwrap();
                 println!("{}", x);
-                Some(self.pc + 1)
+            },
+            Insn::Enter(n) => {
+                for i in 0..n {
+                    self.locals.push(0 as i64);
+                }
+                self.locals.push(n as i64);
+            },
+            Insn::Exit => {
+                let n = self.locals.pop().unwrap();
+                for i in 0..n {
+                    self.locals.pop();
+                }
+            },
+            Insn::GetLocal(x) => {
+                // len() - 1 contains number of locals in this block.
+                // len() - 1 - 1 contains local #1
+                // len() - 1 - i contains local #i
+                let index = self.locals.len() - 1 - x;
+                let val = self.locals[index];
+                self.stack.push(val);
+            },
+            Insn::SetLocal(x) => {
+                let index = self.locals.len() - 1 - x;
+                let val = self.stack.pop().unwrap();
+                self.locals[index] = val;
             },
         }
+        Some(self.pc + 1)
     }
     
     pub fn execute(&mut self) {
@@ -112,6 +149,8 @@ fn main() {
 
     let mut code = Vec::new();
     compile_exp(&e, &mut code);
+    code.push(Insn::Print);
+    code.push(Insn::Halt);
     println!("{:?}", code);
 
     let mut vm = VM::new(code);
