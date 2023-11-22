021295e1b78facf0e748bcc91a0c62522d9346a7
Robert J Nagel <rjnagel64@gmail.com>
2023-11-21 21:01:12 -0600

    working example of egraph opt -- slightly underwhelming

--- src/egraph.rs
+++ src/egraph.rs
@@ -57,13 +57,20 @@ use egg::Id;
 use egg::Symbol;
 use egg::{rewrite, define_language};
 
-use crate::syntax::{Block, Stmt, Expr, BinOp, Var};
+use crate::syntax::{Block, Stmt, Expr, BinOp, Var, Program};
+use crate::parser::ProgramParser;
 
 use std::collections::HashMap;
 
 define_language! {
     enum GraphExpr {
+        // Numeric constants
         Num(i64),
+        // Abstract symbols. IIUC these would arise from (e.g.) function parameters that don't have
+        // a specific value. I don't have functions, but it doesn't hurt anything to leave this
+        // constructor here.
+        Symbol(Symbol),
+
         // Hmm. It seems I have to unpack Expr::BinOp into each of its variants, due to limits of
         // the define_language! macro.
         "+" = Add([Id; 2]),
@@ -72,12 +79,14 @@ define_language! {
         "<" = Lt([Id; 2]),
         ">" = Gt([Id; 2]),
 
+        // Read the argument at position <i> from the arguments array.
+        "args" = ArgRef(Id),
+
         // '[]' is a kind of dummy value, representing "perform no IO operations" (i.e., trivial
         // effect, pure ())
         "[]" = IOInit,
         // 'e1 >>> e2' means "perform operations from e1 and then print e2"
         ">>>" = IOSeq([Id; 2]),
-        Symbol(Symbol),
     }
 }
 
@@ -127,7 +136,7 @@ impl EGraphBuilder {
         }
     }
 
-    fn simple_block_to_egraph(&mut self, block: Block) {
+    fn block_to_egraph(&mut self, block: &Block) {
         for s in &block.0 {
             match s {
                 Stmt::Assign(x, e) => {
@@ -139,7 +148,7 @@ impl EGraphBuilder {
                     //
                     // The tricky part is how I merge things back together. I don't know how to
                     // represent a phi-function in an egraph
-                    *self.env.get_mut(x).unwrap() = id;
+                    self.env.insert(x.clone(), id);
 
                     // Actually, what's GraphExpr::Symbol for? opaque variables that don't have a
                     // know definition? (e.g., function parameters or user input?)
@@ -148,6 +157,11 @@ impl EGraphBuilder {
                     let id = self.expression_to_egraph(e);
                     self.io_root = self.graph.add(GraphExpr::IOSeq([self.io_root, id]));
                 },
+                Stmt::Input(x, e) => {
+                    let arg_id = self.expression_to_egraph(e);
+                    let exp_id = self.graph.add(GraphExpr::ArgRef(arg_id));
+                    self.env.insert(x.clone(), exp_id);
+                },
                 _ => {
                     unimplemented!("complex statements to egraph")
                 },
@@ -155,44 +169,94 @@ impl EGraphBuilder {
         }
     }
 
+    fn program_to_egraph(&mut self, prog: &Program) {
+        self.block_to_egraph(&prog.0)
+    }
+
 }
 
 // Cyclic expressions: use EGraph::union(i, j) to equate i and j, creating a cycle?
 
+fn run_program(src: &str, args: Vec<i64>) {
+    let parser = ProgramParser::new();
+    let prog = parser.parse(src).expect("valid syntax");
+
+    let mut com = EGraphBuilder::new();
+    com.program_to_egraph(&prog);
+
+    let rules: &[Rewrite<GraphExpr, ()>] = &[
+        rewrite!("add-comm"; "(+ ?x ?y)" => "(+ ?y ?x)"),
+        rewrite!("mult-comm"; "(* ?x ?y)" => "(* ?y ?x)"),
+        rewrite!("add-0"; "(+ ?x 0)" => "?x"),
+        rewrite!("mul-1"; "(* ?x 1)" => "?x"),
+        rewrite!("sub-self"; "(- ?x ?x)" => "0"),
+        rewrite!("mult-dist-add"; "(* ?a (+ ?b ?c))" => "(+ (* ?a ?b) (* ?a ?c))"),
+    ];
+
+    let program_root = com.io_root;
+    let runner = Runner::default().with_egraph(com.graph).run(rules);
+    let extractor = Extractor::new(&runner.egraph, egg::AstSize);
+    let (_best_cost, best_expr) = extractor.find_best(program_root);
+    println!("{}", best_expr);
+
+    // Okay, now I need some sort of "linearizer" that turns an egraph-expression back into a
+    // program.
+    //
+    // Also, it would be good to have some form of CSE/hash-consing. It seems that the extractor
+    // tends to duplicate subexpressions.
+
+    // let mut com = Compiler::new();
+    // com.compile_program(&p);
+    //
+    // let code = com.output();
+    // println!("--- Compiled bytecode: ---");
+    // println!("{:?}", code);
+    //
+    // let mut vm = VM::new(code, args);
+    //
+    // println!("--- Results: ---");
+    // vm.execute();
+    // vm.dump_state();
+}
+
 pub fn demo() {
     println!("EGG");
 
-    let mut expr = RecExpr::default();
-    let a1 = expr.add(SymbolLang::leaf("a"));
-    let b1 = expr.add(SymbolLang::leaf("0"));
-    let foo1 = expr.add(SymbolLang::new("+", vec![a1, b1]));
-    let a2 = expr.add(SymbolLang::leaf("0"));
-    let b2 = expr.add(SymbolLang::leaf("b"));
-    let foo2 = expr.add(SymbolLang::new("+", vec![a2, b2]));
-    let bar = expr.add(SymbolLang::new("*", vec![foo1, foo2]));
-    println!("this expr is {}", expr);
+    let src_filename = "demo.prog";
+    let src = std::fs::read_to_string(src_filename).expect("src file does not exist");
+    run_program(&src, vec![3]);
 
-    // let mut egraph = EGraph::new(());
-    // let a1 = egraph.add(SymbolLang::leaf("a"));
-    // let b1 = egraph.add(SymbolLang::leaf("0"));
-    // let foo1 = egraph.add(SymbolLang::new("+", vec![a1, b1]));
-    // let a2 = egraph.add(SymbolLang::leaf("a"));
-    // let b2 = egraph.add(SymbolLang::leaf("0"));
-    // let foo2 = egraph.add(SymbolLang::new("+", vec![a2, b2]));
-    // let bar = egraph.add(SymbolLang::new("*", vec![foo1, foo2]));
-    // println!("{:?}", egraph.dump());
-
-
-    let rules: &[Rewrite<SymbolLang, ()>] = &[
-        rewrite!("add-comm"; "(+ ?x ?y)" => "(+ ?y ?x)"),
-        rewrite!("add-0"; "(+ ?x 0)" => "?x"),
-    ];
-
-    // let runner = Runner::default().with_egraph(egraph).run(rules);
-    let runner = Runner::default().with_expr(&expr).run(rules);
-    let extractor = Extractor::new(&runner.egraph, egg::AstSize);
-    let (_best_cost, best_expr) = extractor.find_best(runner.roots[0]);
-    println!("{}", best_expr);
+    // let mut expr = RecExpr::default();
+    // let a1 = expr.add(SymbolLang::leaf("a"));
+    // let b1 = expr.add(SymbolLang::leaf("0"));
+    // let foo1 = expr.add(SymbolLang::new("+", vec![a1, b1]));
+    // let a2 = expr.add(SymbolLang::leaf("0"));
+    // let b2 = expr.add(SymbolLang::leaf("b"));
+    // let foo2 = expr.add(SymbolLang::new("+", vec![a2, b2]));
+    // let bar = expr.add(SymbolLang::new("*", vec![foo1, foo2]));
+    // println!("this expr is {}", expr);
+    //
+    // // let mut egraph = EGraph::new(());
+    // // let a1 = egraph.add(SymbolLang::leaf("a"));
+    // // let b1 = egraph.add(SymbolLang::leaf("0"));
+    // // let foo1 = egraph.add(SymbolLang::new("+", vec![a1, b1]));
+    // // let a2 = egraph.add(SymbolLang::leaf("a"));
+    // // let b2 = egraph.add(SymbolLang::leaf("0"));
+    // // let foo2 = egraph.add(SymbolLang::new("+", vec![a2, b2]));
+    // // let bar = egraph.add(SymbolLang::new("*", vec![foo1, foo2]));
+    // // println!("{:?}", egraph.dump());
+    //
+    //
+    // let rules: &[Rewrite<SymbolLang, ()>] = &[
+    //     rewrite!("add-comm"; "(+ ?x ?y)" => "(+ ?y ?x)"),
+    //     rewrite!("add-0"; "(+ ?x 0)" => "?x"),
+    // ];
+    //
+    // // let runner = Runner::default().with_egraph(egraph).run(rules);
+    // let runner = Runner::default().with_expr(&expr).run(rules);
+    // let extractor = Extractor::new(&runner.egraph, egg::AstSize);
+    // let (_best_cost, best_expr) = extractor.find_best(runner.roots[0]);
+    // println!("{}", best_expr);
 
     // hmm. translation of straight-line to egraph:
     // maintain map of Var -> Id
